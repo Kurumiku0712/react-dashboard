d3d831e2c93105b29388ef3f01db4cdcbcf108f0
Kurumiku0712 <kurumiku0712@gmail.com>
2025-02-19 11:10:21 +1100

    chore: Update eslint.config.js for Jest

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import jest from "eslint-plugin-jest";
 
 export default [
   { ignores: ["dist"] },
@@ -10,7 +11,11 @@ export default [
     files: ["**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
       parserOptions: {
         ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
@@ -22,16 +27,18 @@ export default [
       react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      jest,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
+      ...jest.configs.recommended.rules,
 
       "no-unused-vars": "off",
       "react/prop-types": "off",
-      
+
       "react/jsx-no-target-blank": "off",
       "react-refresh/only-export-components": [
         "warn",
